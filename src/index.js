d94cc6962d97a0f3d0e8137e9494a5e0e8fc169b
coke12103 <coke12103@gmail.com>
2020-04-07 02:15:29 +0900

    add visibility select(#9)

--- src/index.js
+++ src/index.js
@@ -3,23 +3,17 @@ const {
   QMainWindow,
   QLabel,
   FlexLayout,
-  QPlainTextEdit,
-  QPushButton,
-  QPushButtonEvents,
   QWidget,
-  QLineEdit,
   QApplication,
-  QKeyEvent,
-  KeyboardModifier,
-  Key,
   QFont
 } = require('@nodegui/nodegui');
 
 const file = require('./file.js');
-const style = require('./style.js');
+const Assets = require('./assets.js');
 const _timeline = require('./timeline.js');
 const _checkboxs = require('./checkboxs.js');
 const _post_view_area = require('./postview.js');
+const _post_box = require('./postbox/index.js');
 const Client = require('./client.js');
 const client = new Client();
 const default_font = new QFont('sans', 9);
@@ -33,71 +27,42 @@ const rootViewLayout = new FlexLayout();
 rootView.setObjectName('rootView');
 rootView.setLayout(rootViewLayout);
 
-const postArea = new QWidget();
-const postAreaLayout = new FlexLayout();
-postArea.setObjectName('postArea');
-postArea.setLayout(postAreaLayout);
-
 const statusLabel = new QLabel();
 statusLabel.setWordWrap(true);
 statusLabel.setFont(default_font);
 statusLabel.setText('ログインチェック中...');
 statusLabel.setObjectName('statusLabel');
 
-const postTextInput = new QPlainTextEdit();
-postTextInput.setObjectName('postTextInput');
-postTextInput.setReadOnly(false);
-postTextInput.setWordWrapMode(3);
-postTextInput.setFont(default_font);
-postTextInput.setPlaceholderText('言いたいことは？');
-
-postAreaLayout.addWidget(postTextInput);
-
 const timelineControlsArea = new QWidget();
 const timelineControlsAreaLayout = new FlexLayout();
 timelineControlsArea.setObjectName('timelineControlsArea');
 timelineControlsArea.setLayout(timelineControlsAreaLayout);
 
-const postButton = new QPushButton();
-postButton.setText('Post!');
-postButton.setObjectName('postButton');
-postAreaLayout.addWidget(postButton);
-
-postButton.addEventListener('clicked', () =>{
-  var body = postTextInput.toPlainText();
-
-  if(!body){
-    statusLabel.setText('本文入れてね');
-    return;
-  }
-
-  var data = { text: body };
-  statusLabel.setText('投稿中...');
-  client.call('notes/create', data).then(() => {
-      statusLabel.setText('投稿成功!');
-      postTextInput.setPlainText('');
-      postTextInput.update();
-  }).catch((err) => {
-      console.log(err);
-      statusLabel.setText(err.error.error.message);
-  });
-});
-
-postTextInput.addEventListener('KeyPress', (key) => {
-    var qkey = new QKeyEvent(key);
-    if(qkey.modifiers() != KeyboardModifier.ControlModifier) return;
-    if(!(
-        qkey.key() == Key.Key_Enter ||
-        qkey.key() == Key.Key_Return
-    )) return;
-
-    postButton.click();
-});
-
 var timeline = new _timeline();
 var postViewArea = new _post_view_area();
 var checkboxs = new _checkboxs();
 var timeline_auto_select = checkboxs.get('timeline_auto_select');
+var postbox = new _post_box();
+var assets = new Assets('MainWindow');
+
+postbox.add_event_listener(async () => {
+    var data = postbox.get_data();
+    console.log(data)
+    if(!data.text){
+      statusLabel.setText('本文を入れてね');
+      return;
+    }
+
+    statusLabel.setText('投稿中...');
+    try{
+      await client.call('notes/create', data);
+      postbox.clear();
+      statusLabel.setText('投稿成功!');
+    }catch(err){
+      console.log(err);
+      statusLabel.setText(err.error.error.message);
+    }
+})
 
 timeline.set_auto_select_check(timeline_auto_select);
 timeline.set_post_view(postViewArea);
@@ -107,10 +72,10 @@ timelineControlsAreaLayout.addWidget(timeline_auto_select);
 rootViewLayout.addWidget(postViewArea.get_widget());
 rootViewLayout.addWidget(timeline.get_widget());
 rootViewLayout.addWidget(timelineControlsArea);
-rootViewLayout.addWidget(postArea);
+rootViewLayout.addWidget(postbox.area);
 rootViewLayout.addWidget(statusLabel);
 
-style.add_style(rootView, './style/index.css');
+rootView.setStyleSheet(assets.css);
 
 win.setCentralWidget(rootView);
 win.show();
---
+++ src/postbox/index.js
@@ -0,0 +1,122 @@
+const {
+  QWidget,
+  FlexLayout,
+  QPlainTextEdit,
+  QPushButton,
+  QKeyEvent,
+  KeyboardModifier,
+  Key,
+  QFont,
+  QComboBox,
+  QCheckBox
+} = require('@nodegui/nodegui');
+
+class PostBox{
+  constructor(){
+    const font = new QFont('sans', 9);
+
+    const Area = new QWidget();
+    const AreaLayout = new FlexLayout();
+    Area.setObjectName('postArea');
+    Area.setLayout(AreaLayout);
+
+    const SubArea = new QWidget();
+    const SubAreaLayout = new FlexLayout();
+    SubArea.setObjectName('postSubArea');
+    SubArea.setLayout(SubAreaLayout);
+
+    const postTextInput = new QPlainTextEdit();
+    postTextInput.setObjectName('postTextInput');
+    postTextInput.setReadOnly(false);
+    postTextInput.setWordWrapMode(3);
+    postTextInput.setFont(font);
+    postTextInput.setPlaceholderText('言いたいことは？');
+
+    const postButton = new QPushButton();
+    postButton.setText('Post!');
+    postButton.setObjectName('postButton');
+    postButton.setFont(font);
+
+    const visibilitySelect = new QComboBox();
+    visibilitySelect.setObjectName('postVisibilitySelect');
+    visibilitySelect.setFont(font);
+    visibilitySelect.addItem(undefined, '公開');
+    visibilitySelect.addItem(undefined, 'ホーム');
+    visibilitySelect.addItem(undefined, 'フォロワー');
+    visibilitySelect.addItem(undefined, 'ダイレクト');
+    visibilitySelect.addItem(undefined, 'ランダム');
+
+    const isLocalCheck = new QCheckBox();
+    isLocalCheck.setObjectName('postIsLocalCheck');
+    isLocalCheck.setText('ローカルのみ');
+    isLocalCheck.setFont(font);
+
+    SubAreaLayout.addWidget(postButton);
+    SubAreaLayout.addWidget(visibilitySelect);
+    SubAreaLayout.addWidget(isLocalCheck);
+
+    AreaLayout.addWidget(postTextInput);
+    AreaLayout.addWidget(SubArea);
+
+    this.font = font;
+    this.area = Area;
+    this.layout = AreaLayout;
+    this.post_text_input = postTextInput;
+    this.post_button = postButton;
+    this.visibility_select = visibilitySelect;
+    this.is_local_check = isLocalCheck;
+  }
+
+  get_data(){
+    var text = this.post_text_input.toPlainText();
+    var _visibility = this.visibility_select.currentText();
+    var is_local = this.is_local_check.isChecked();
+    var visibility = 'public';
+
+    switch(_visibility){
+      case "公開":
+        visibility = 'public';
+        break;
+      case 'ホーム':
+        visibility = 'home';
+        break;
+      case 'フォロワー':
+        visibility = 'followers';
+        break;
+      case 'ダイレクト':
+        visibility = 'specified';
+        break;
+      case 'ランダム':
+        var __visibility = ['public', 'home', 'followers', 'specified'];
+        visibility = __visibility[Math.floor(Math.random() * __visibility.length)];
+    }
+    var data = {
+      text: text,
+      visibility: visibility,
+      localOnly: is_local
+    }
+
+    return data;
+  };
+  add_event_listener(callback){
+    this.post_button.addEventListener('clicked', () => {
+        callback();
+    });
+    this.post_text_input.addEventListener('KeyPress', (key) => {
+        var _key = new QKeyEvent(key);
+        if(_key.modifiers() != KeyboardModifier.ControlModifier) return;
+        if(!(
+            _key.key() == Key.Key_Enter ||
+            _key.key() == Key.Key_Return
+        )) return;
+
+        this.post_button.click();
+    })
+  }
+  clear(){
+    this.post_text_input.setPlainText('');
+    this.post_text_input.update();
+  };
+}
+
+module.exports = PostBox;
