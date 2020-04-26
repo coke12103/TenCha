7573440c0e47624e32ba8fb1739ead0ae699d944
coke12103 <coke12103@gmail.com>
2020-04-26 16:24:20 +0900

    change url regexp

--- src/tools/post_parser/index.js
+++ src/tools/post_parser/index.js
@@ -3,7 +3,7 @@ class PostParser{
   }
   parse(text){
     var break_regexp = new RegExp('\n', 'g');
-    var url_regexp = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))/gi;
+    var url_regexp = new RegExp('((?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?)', 'gi');
 
 
     text = text.replace(break_regexp, '<br>');
