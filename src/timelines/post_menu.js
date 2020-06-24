d230ff65d30d5335ef362ea8307d47f7c794270e
coke12103 <coke12103@gmail.com>
2020-06-25 06:32:11 +0900

    add note delete

--- src/timelines/post_menu.js
+++ src/timelines/post_menu.js
@@ -11,11 +11,13 @@ class PostMenu{
     const quote_action = new QAction();
     const reply_action = new QAction();
     const image_view_action = new QAction();
+    const note_remove_action = new QAction();
 
     renote_action.setText('Renote');
     quote_action.setText('引用Renote');
     reply_action.setText('リプライ');
     image_view_action.setText('画像を表示');
+    note_remove_action.setText('削除');
 
     menu.addAction(reply_action);
     menu.addSeparator(renote_action);
@@ -23,12 +25,15 @@ class PostMenu{
     menu.addAction(quote_action);
     menu.addSeparator(image_view_action);
     menu.addAction(image_view_action);
+    menu.addSeparator(note_remove_action);
+    menu.addAction(note_remove_action);
 
     this.menu = menu;
     this.renote_action = renote_action;
     this.quote_action = quote_action;
     this.reply_action = reply_action;
     this.image_view_action = image_view_action;
+    this.note_remove_action = note_remove_action;
   }
 
   get_widget(){
@@ -50,10 +55,14 @@ class PostMenu{
     this.reply_action.addEventListener('triggered', () => {
         this.post_action.reply();
     });
+    this.note_remove_action.addEventListener('triggered', () => {
+        this.post_action.note_remove();
+    });
   }
 
   exec(pos){
     this.image_view_action.setEnabled(this.post_action.is_image_view_ready());
+    this.note_remove_action.setEnabled(this.post_action.is_remove_ready());
 
     this.menu.exec(pos);
   }
