0374ceb4b189daac041968b8166aeac4ec6f8b67
coke12103 <coke12103@gmail.com>
2020-08-11 23:07:28 +0900

    add IconLabel widget

---
+++ src/widgets/icon_label/index.js
@@ -0,0 +1,39 @@
+const {
+  QLabel
+} = require('@nodegui/nodegui');
+
+class IconLabel extends QLabel{
+  constructor(size){
+    super();
+    this.maxSize = size;
+
+    this.setMinimumSize(this.maxSize, this.maxSize);
+    this.setFixedSize(this.maxSize, this.maxSize);
+  }
+
+  setPixmap(pix){
+    var width = pix.width();
+    var height = pix.height();
+
+    if(width > height){
+      var ratio = width / this.maxSize;
+
+      width = this.maxSize;
+      height = height / ratio;
+    }else{
+      var ratio = height / this.maxSize;
+
+      width = width / ratio;
+      height = this.maxSize;
+    }
+
+    width = Math.ceil(width);
+    height = Math.ceil(height);
+
+    var icon = pix.scaled(width, height);
+
+    super.setPixmap(icon);
+  }
+}
+
+module.exports = IconLabel;
