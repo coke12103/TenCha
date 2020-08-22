e228a72ca4cac75e5c2bc9a3c12f7e4ddffa373b
coke12103 <coke12103@gmail.com>
2020-08-22 23:53:24 +0900

    rewrite login

---
+++ src/widgets/login_window/login_url_widget.js
@@ -0,0 +1,52 @@
+const {
+  QWidget,
+  QLabel,
+  QBoxLayout,
+  Direction,
+  QPushButton,
+  QLineEdit,
+  WindowType,
+  TextInteractionFlag
+} = require('@nodegui/nodegui');
+
+class LoginUrlWidget extends QWidget{
+  constructor(){
+    super();
+
+    this.layout = new QBoxLayout(Direction.TopToBottom);
+
+    this.message_label = new QLabel();
+    this.status_label = new QLabel();
+    this.link_label = new QLabel();
+    this.confirm_button = new QPushButton();
+
+    this.setLayout(this.layout);
+
+    this.layout.setContentsMargins(5,5,5,5);
+    this.layout.setSpacing(5);
+
+    this.message_label.setWordWrap(true);
+    this.message_label.setText('アプリケーションの作成に成功しました!\nURLにアクセスしてアクセス許可をして「やっていく」ボタンを押してください!');
+    this.message_label.setObjectName('loginLabel');
+
+    this.status_label.setText('');
+    this.status_label.setWordWrap(true);
+    this.status_label.setObjectName('statusLabel');
+
+    this.link_label.setWordWrap(true);
+    this.link_label.setTextInteractionFlags(TextInteractionFlag.LinksAccessibleByMouse);
+    this.link_label.setOpenExternalLinks(true);
+    this.link_label.setObjectName('link');
+
+    this.confirm_button.setText('やっていく');
+    this.confirm_button.setObjectName('postButton');
+
+    this.layout.addWidget(this.message_label);
+    this.layout.addWidget(this.status_label);
+    this.layout.addWidget(this.link_label);
+    this.layout.addStretch(1);
+    this.layout.addWidget(this.confirm_button);
+  }
+}
+
+module.exports = LoginUrlWidget;
