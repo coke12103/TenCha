460ce7e57cbe3ccbcfc48d0efe43c64af7d2ec19
coke12103 <coke12103@gmail.com>
2020-09-12 19:32:55 +0900

    use data directory

--- src/blocker/domain.js
+++ src/blocker/domain.js
@@ -1,16 +1,14 @@
 const file = require('../file.js');
+const App = require('../index.js');
 
 class DomainBlocker{
   constructor(){
-  }
-
-  async init(){
-    if(!file.exist_check('./domain_blocks.json')){
-      await this.create_default_file();
+    if(!file.exist_check(`${App.data_directory.get('ng_settings')}domain_blocks.json`)){
+      this.create_default_file();
     }
 
     try{
-      var f = file.load('./domain_blocks.json');
+      var f = file.load(`${App.data_directory.get('ng_settings')}domain_blocks.json`);
       f = JSON.parse(f);
       this.block_domains = f.block_domains;
     }catch(err){
@@ -19,20 +17,19 @@ class DomainBlocker{
     }
   }
 
-  async create_default_file(){
+  create_default_file(){
     var default_file = {
       block_domains: []
     };
 
-    await file.json_write('./domain_blocks.json', default_file);
+    file.json_write_sync(`${App.data_directory.get('ng_settings')}domain_blocks.json`, default_file);
   }
 
   is_block(note){
-    var host = note.user.host;
-
     var result = false;
     for(var domain of this.block_domains){
-      if(host == domain) result = true;
+      if(note.user.host == domain) result = true;
+      if(note.renote && note.renote.user.host == domain) result = true;
     }
 
     return result;
