8aa52eff84f1e7e66683673f2f0c7e329183ef35
coke12103 <coke12103@gmail.com>
2020-09-22 09:21:47 +0900

    add local only flag

--- src/widgets/postview/flag_widget.js
+++ src/widgets/postview/flag_widget.js
@@ -25,11 +25,16 @@ class FlagWidget extends QWidget{
     this.lock = new QLabel();
     this._public = new QLabel()
     this.renote = new QLabel();
+    // TODO: アイコンを用意する。とりあえず色の差分で代用はするけど。
+    this.local_home = new QLabel();
+    this.local_lock = new QLabel();
+    this.local_public = new QLabel()
+    this.local_direct = new QLabel();
 
     this.setLayout(this.layout);
 
     this.layout.setContentsMargins(0,0,0,0);
-    this.layout.setSpacing(5);
+    this.layout.setSpacing(4);
 
     var clip_pix = new QPixmap(Icons.clip);
     var direct_pix = new QPixmap(Icons.direct);
@@ -38,56 +43,90 @@ class FlagWidget extends QWidget{
     var public_pix = new QPixmap(Icons._public);
     var renote_pix = new QPixmap(Icons.renote);
 
+    direct_pix = direct_pix.scaled(
+      IconsSize, IconsSize,
+      AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
+    );
+
+    home_pix = home_pix.scaled(
+      IconsSize, IconsSize,
+      AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
+    );
+
+    lock_pix = lock_pix.scaled(
+      IconsSize, IconsSize,
+      AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
+    );
+
+    public_pix = public_pix.scaled(
+      IconsSize, IconsSize,
+      AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
+    );
+
+
     this.clip.setPixmap(clip_pix.scaled(
         IconsSize, IconsSize,
         AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
       )
     );
-    this.direct.setPixmap(direct_pix.scaled(
-        IconsSize, IconsSize,
-        AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
-      )
-    );
-    this.home.setPixmap(home_pix.scaled(
-        IconsSize, IconsSize,
-        AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
-      )
-    );
-    this.lock.setPixmap(lock_pix.scaled(
-        IconsSize, IconsSize,
-        AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
-      )
-    );
-    this._public.setPixmap(public_pix.scaled(
-        IconsSize, IconsSize,
-        AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
-      )
-    );
+
     this.renote.setPixmap(renote_pix.scaled(
         IconsSize, IconsSize,
         AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
       )
     );
 
-    this.layout.addStretch(1);
-    this.layout.addWidget(this.renote);
-    this.layout.addWidget(this.clip);
+    this.direct.setPixmap(direct_pix);
 
-    this.layout.addWidget(this.direct);
-    this.layout.addWidget(this.lock);
-    this.layout.addWidget(this.home);
+    this.local_direct.setPixmap(direct_pix);
+    this.local_direct.setEnabled(false);
+
+    this.home.setPixmap(home_pix);
+
+    this.local_home.setPixmap(home_pix);
+    this.local_home.setEnabled(false);
+
+    this.lock.setPixmap(lock_pix);
+
+    this.local_lock.setPixmap(lock_pix);
+    this.local_lock.setEnabled(false);
+
+    this._public.setPixmap(public_pix);
+
+    this.local_public.setPixmap(public_pix);
+    this.local_public.setEnabled(false);
+
+    this.layout.addStretch(1);
+
+    this.layout.addWidget(this.clip);
+    this.layout.addWidget(this.renote);
+
     this.layout.addWidget(this._public);
+    this.layout.addWidget(this.home);
+    this.layout.addWidget(this.lock);
+    this.layout.addWidget(this.direct);
+
+    this.layout.addWidget(this.local_public);
+    this.layout.addWidget(this.local_home);
+    this.layout.addWidget(this.local_lock);
+    this.layout.addWidget(this.local_direct);
 
     this.layout.addStretch(1);
   }
 
   clear(){
     this.clip.hide();
-    this.direct.hide();
+    this.renote.hide();
+
+    this._public.hide();
     this.home.hide();
     this.lock.hide();
-    this._public.hide();
-    this.renote.hide();
+    this.direct.hide();
+
+    this.local_public.hide();
+    this.local_home.hide();
+    this.local_lock.hide();
+    this.local_direct.hide();
   }
 
   setNoteFlag(note){
@@ -98,16 +137,20 @@ class FlagWidget extends QWidget{
 
     switch(note.visibility){
       case 'public':
-        this._public.show();
+        if(note.localOnly) this.local_public.show();
+        else this._public.show();
         break;
       case 'home':
-        this.home.show();
+        if(note.localOnly) this.local_home.show();
+        else this.home.show();
         break;
       case 'followers':
-        this.lock.show();
+        if(note.localOnly) this.local_lock.show();
+        else this.lock.show();
         break;
       case 'specified':
-        this.direct.show();
+        if(note.localOnly) this.local_direct.show();
+        else this.direct.show();
         break;
       default:
         break
