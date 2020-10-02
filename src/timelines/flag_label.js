4f08884397b49bb7bf8cdf7d6825b5933c38f8f8
coke12103 <coke12103@gmail.com>
2020-10-02 14:57:49 +0900

    add flag label

---
+++ src/timelines/flag_label.js
@@ -0,0 +1,63 @@
+const {
+  QLabel,
+  QPixmap,
+  TransformationMode,
+  AspectRatioMode
+} = require('@nodegui/nodegui');
+
+const Assets = require("../assets.js");
+const Icons = new Assets('Icons');
+const App = require("../index.js");
+
+const IconsSize = 14;
+
+class FlagLabel extends QLabel{
+  constructor(){
+    super();
+
+    this.setMaximumSize(IconsSize, IconsSize);
+    this.setMinimumSize(IconsSize, IconsSize);
+  }
+
+  setFlag(type){
+    var pixmap = new QPixmap();
+
+    switch(type){
+      case 'follow':
+        pixmap.load(Icons.follow);
+        break;
+      case 'mention':
+      case 'reply':
+        pixmap.load(Icons.reply);
+        break;
+      case 'renote':
+        pixmap.load(Icons.renote);
+        break;
+      case 'quote':
+        pixmap.load(Icons.quote);
+        break;
+      case 'reaction':
+        pixmap.load(Icons.reaction);
+        break;
+      case 'pollVote':
+        pixmap.load(Icons.poll);
+        break;
+      case 'receiveFollowRequest':
+        pixmap.load(Icons.follow_request);
+        break;
+      case 'followRequestAccepted':
+        pixmap.load(Icons.follow_accept);
+        break;
+    }
+
+    pixmap = pixmap.scaled(
+      IconsSize, IconsSize,
+      AspectRatioMode.KeepAspectRatio, TransformationMode.SmoothTransformation
+    );
+
+    this.setPixmap(pixmap);
+  }
+}
+
+module.exports = FlagLabel;
+
