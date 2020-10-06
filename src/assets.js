1742c81ba1e6f1b309364f9f356f84f7519978c6
coke12103 <coke12103@gmail.com>
2020-10-06 13:57:40 +0900

    remove old asset template

--- src/assets.js
+++ src/assets.js
@@ -4,9 +4,6 @@ class Asset{
       case "PostView":
         this.no_image = require('../assets/no_image.png').default;
         break;
-      case "MainWindow":
-        this.css = require('!!raw-loader!../assets/css/index.css').default;
-        break;
       case "LoginWindow":
         this.css = require('!!raw-loader!../assets/css/login.css').default;
         break;
